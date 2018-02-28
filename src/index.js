c593d297bc68309899b5973f026e22bf791e4a4f
Skifling <it.labkovich@gmail.com>
2018-02-28 15:43:25 +0300

    16 from 20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+    var fLenBC = bracketsConfig.length;
+    var fLenStr = str.length;
+    var count = 0;
+    for (i = 0; i < fLenBC; i++) {
+        for (j = 0; j < fLenStr; j++) {
+            if (str[j] == bracketsConfig[i][0]) {
+                count++;
+            } 
+            if (str[j] == bracketsConfig[i][1]) {
+                count--;
+            }
+            if (count < 0) { return false;}
+        }
+        if (count != 0) { return false;}
+    }
+    return true;
 }
